7444aa4931a04c280b7b98c6a3ef5d14ccf62d4c
Brandon <juhkim90@gmail.com>
2021-01-15 00:39:19 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Input
+
+Input is the way the computer gets information from the user
+
+Python initially treats input as a __string__ type
+
+*Ex.*
+5 --> "5"
+Today is Friday --> "Today is Friday" 
+
+### Formula: 
+`variableName = input("Optional Prompt")`
+
+Python stores the __input__ in the __variableName__
---
+++ main.py
@@ -0,0 +1,17 @@
+name = input("Hey! What's your name? ")
+print("Oh hello " + name + "! That's a cool name!")
+
+year = input("Last thing! What year were you born? ")
+print(year + " was sooooo long ago")
+
+# Ask user for school 
+# Ask user for favorite superhero 
+
+# Challenge:
+# Ask user for the year they were born, and print it out
+
+school = input("What school do you go to? ")
+print("Hmmm I've never heard of " + school)
+
+superhero = input("Okay whats your favorite superhero? ")
+print("No way! " + superhero + " is my favorite too!")
